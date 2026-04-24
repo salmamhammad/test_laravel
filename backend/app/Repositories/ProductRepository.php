d674cc492c11791c5d960a42a4e7a43807296573
salma <salmamhammmad@gmail.com>
2026-04-24 22:55:27 +0300

    improve the pages with css

--- backend/app/Repositories/ProductRepository.php
+++ backend/app/Repositories/ProductRepository.php
@@ -9,11 +9,12 @@ class ProductRepository implements ProductRepositoryInterface
     public function getFilteredProducts(array $filters)
     {
         $query = Product::with('category:id,name');
-
+        // список фильтров
+        //фильтр: q
         if (!empty($filters['q'])) {
             $query->where('name', 'LIKE', "%{$filters['q']}%");
         }
-
+        //фильтр: price_from, price_to
         if (!empty($filters['price_from'])) {
             $query->where('price', '>=', $filters['price_from']);
         }
@@ -21,19 +22,19 @@ class ProductRepository implements ProductRepositoryInterface
         if (!empty($filters['price_to'])) {
             $query->where('price', '<=', $filters['price_to']);
         }
-
+         //фильтр: category_id
         if (!empty($filters['category_id'])) {
             $query->where('category_id', $filters['category_id']);
         }
-
+        //фильтр: in_stock (true/false)
         if (isset($filters['in_stock'])) {
             $query->where('in_stock', filter_var($filters['in_stock'], FILTER_VALIDATE_BOOLEAN));
         }
-
+         //фильтр: rating_from
         if (!empty($filters['rating_from'])) {
             $query->where('rating', '>=', $filters['rating_from']);
         }
-
+        //Сортировка
         switch ($filters['sort'] ?? null) {
             case 'price_asc':
                 $query->orderBy('price');
@@ -47,7 +48,7 @@ class ProductRepository implements ProductRepositoryInterface
             default:
                 $query->orderByDesc('created_at');
         }
-
+        //Пагинация
         return $query->paginate(10);
     }
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { fetchProducts, fetchCategories } from "./api";
-
+import "./index.css";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+
   const [filters, setFilters] = useState({
     q: "",
     price_from: "",
@@ -14,94 +16,131 @@ function App() {
     sort: "newest",
     page: 1,
   });
-const [categories, setCategories] = useState([]);
-const [pagination, setPagination] = useState({
-  current_page: 1,
-  last_page: 1,
-});
-useEffect(() => {
-  fetchCategories().then(res => setCategories(res.data));
-}, []);
-const loadProducts = async (customFilters = filters, page = 1) => {
-  const res = await fetchProducts({
-    ...customFilters,
-    page, 
-  });
 
-  setProducts(res.data.data);
-
-  setPagination({
-    current_page: res.data.current_page,
-    last_page: res.data.last_page,
+  const [pagination, setPagination] = useState({
+    current_page: 1,
+    last_page: 1,
   });
-};
 
   useEffect(() => {
-    loadProducts();
+    fetchCategories().then(res => setCategories(res.data));
+    loadProducts(filters);
   }, []);
 
-  const handleChange = (e) => {
-    setFilters({ ...filters, [e.target.name]: e.target.value ,page: 1});
-  };
-const goToPage = (page) => {
-  const newFilters = { ...filters, page };
-  setFilters(newFilters);
-  loadProducts(newFilters, page);
-};
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>Products</h2>
+  const loadProducts = async (customFilters = filters) => {
+    const res = await fetchProducts(customFilters);
 
-      {/* Filters */}
-      <input name="q" placeholder="Search" onChange={handleChange} />
-      <input name="price_from" placeholder="Min price" onChange={handleChange} />
-      <input name="price_to" placeholder="Max price" onChange={handleChange} />
-      <select name="category_id" onChange={handleChange}>
-        <option value="">All Categories</option>
-        {categories.map(c => (
-          <option key={c.id} value={c.id}>
-            {c.name}
-          </option>
-        ))}
-      </select>
-      <select name="in_stock" onChange={handleChange}>
-         <option value="">All</option>
-         <option value="true">In Stock</option>
-         <option value="false">Out of Stock</option>
-      </select>
-      <input
-        name="rating_from"
-        placeholder="Min rating (0-5)"
-        type="number"
-        step="0.1"
-        min="0"
-        max="5"
-        onChange={handleChange}
-      />
-      <button onClick={() => loadProducts()}>Apply</button>
-        <br/>
+    setProducts(res.data.data);
+
+    setPagination({
+      current_page: res.data.current_page,
+      last_page: res.data.last_page,
+    });
+  };
+
+  const handleChange = (e) => {
+    const newFilters = {
+      ...filters,
+      [e.target.name]: e.target.value,
+      page: 1,
+    };
+
+    setFilters(newFilters);
+  };
+
+  const goToPage = (page) => {
+    const newFilters = { ...filters, page };
+    setFilters(newFilters);
+    loadProducts(newFilters);
+  };
+
+  return (
+    <div className="container">
+
+      <h2 className="title">Product Dashboard</h2>
+
+      {/* FILTERS */}
+      <div className="filter-box">
+
+        <input name="q" placeholder="Search..." onChange={handleChange} className="input" />
+
+        <input name="price_from" placeholder="Min price" onChange={handleChange} className="input" />
+
+        <input name="price_to" placeholder="Max price" onChange={handleChange} className="input" />
+
+        <select name="category_id" onChange={handleChange} className="input">
+          <option value="">All Categories</option>
+          {categories.map(c => (
+            <option key={c.id} value={c.id}>{c.name}</option>
+          ))}
+        </select>
+
+        <select name="in_stock" onChange={handleChange} className="input">
+          <option value="">All Stock</option>
+          <option value="true">In Stock</option>
+          <option value="false">Out</option>
+        </select>
+
+        <input
+          name="rating_from"
+          placeholder="Min rating"
+          type="number"
+          min="0"
+          max="5"
+          step="0.1"
+          onChange={handleChange}
+          className="input"
+        />
+
+        <button onClick={() => loadProducts(filters)} className="btn">
+          Apply
+        </button>
+
+        {/* SORTING */}
         <select
           value={filters.sort}
           onChange={(e) => {
-            const newFilters = { ...filters, sort: e.target.value };
+            const newFilters = { ...filters, sort: e.target.value, page: 1 };
             setFilters(newFilters);
-            loadProducts(newFilters); // 🔥 immediate request
+            loadProducts(newFilters);
           }}
+          className="input"
         >
           <option value="newest">Newest</option>
           <option value="price_asc">Price ↑</option>
           <option value="price_desc">Price ↓</option>
           <option value="rating_desc">Rating</option>
         </select>
-      {/* List */}
-      <ul>
-        {products.map((p) => (
-          <li key={p.id}>
-            {p.name} - price: ${p.price} - rate: {p.rating}  - {p.category?.name}
-          </li>
-        ))}
-      </ul>
-      <div style={{ marginTop: 20 }}>
+
+      </div>
+
+      {/* TABLE */}
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Price</th>
+            <th>Stock</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {products.map(p => (
+            <tr key={p.id}>
+              <td>{p.name}</td>
+              <td>{p.category?.name}</td>
+              <td>${p.price}</td>
+              <td>{p.in_stock ? "Yes" : "No"}</td>
+              <td>{p.rating}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {/* PAGINATION */}
+      <div className="pagination">
         <button
           disabled={pagination.current_page === 1}
           onClick={() => goToPage(pagination.current_page - 1)}
@@ -110,7 +149,7 @@ const goToPage = (page) => {
         </button>
 
         <span>
-          Page {pagination.current_page} of {pagination.last_page}
+          Page {pagination.current_page} / {pagination.last_page}
         </span>
 
         <button
@@ -120,6 +159,7 @@ const goToPage = (page) => {
           Next
         </button>
       </div>
+
     </div>
   );
 }
