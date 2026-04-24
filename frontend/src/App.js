d3eefac4ceae954eb2aa115d91fa2bca1f6dea7e
salma <salmamhammmad@gmail.com>
2026-04-24 22:06:22 +0300

    Implement pagination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,24 +12,42 @@ function App() {
     in_stock: "",
     rating_from: "",
     sort: "newest",
+    page: 1,
   });
 const [categories, setCategories] = useState([]);
+const [pagination, setPagination] = useState({
+  current_page: 1,
+  last_page: 1,
+});
 useEffect(() => {
   fetchCategories().then(res => setCategories(res.data));
 }, []);
-  const loadProducts = async () => {
-    const res = await fetchProducts(filters);
-    setProducts(res.data.data);
-  };
+const loadProducts = async (customFilters = filters, page = 1) => {
+  const res = await fetchProducts({
+    ...customFilters,
+    page, 
+  });
+
+  setProducts(res.data.data);
+
+  setPagination({
+    current_page: res.data.current_page,
+    last_page: res.data.last_page,
+  });
+};
 
   useEffect(() => {
     loadProducts();
   }, []);
 
   const handleChange = (e) => {
-    setFilters({ ...filters, [e.target.name]: e.target.value });
+    setFilters({ ...filters, [e.target.name]: e.target.value ,page: 1});
   };
-
+const goToPage = (page) => {
+  const newFilters = { ...filters, page };
+  setFilters(newFilters);
+  loadProducts(newFilters, page);
+};
   return (
     <div style={{ padding: 20 }}>
       <h2>Products</h2>
@@ -51,12 +69,6 @@ useEffect(() => {
          <option value="true">In Stock</option>
          <option value="false">Out of Stock</option>
       </select>
-      <select name="sort" onChange={handleChange}>
-        <option value="newest">Newest</option>
-        <option value="price_asc">Price ↑</option>
-        <option value="price_desc">Price ↓</option>
-        <option value="rating_desc">Rating</option>
-      </select>
       <input
         name="rating_from"
         placeholder="Min rating (0-5)"
@@ -66,8 +78,21 @@ useEffect(() => {
         max="5"
         onChange={handleChange}
       />
-      <button onClick={loadProducts}>Apply</button>
-
+      <button onClick={() => loadProducts()}>Apply</button>
+        <br/>
+        <select
+          value={filters.sort}
+          onChange={(e) => {
+            const newFilters = { ...filters, sort: e.target.value };
+            setFilters(newFilters);
+            loadProducts(newFilters); // 🔥 immediate request
+          }}
+        >
+          <option value="newest">Newest</option>
+          <option value="price_asc">Price ↑</option>
+          <option value="price_desc">Price ↓</option>
+          <option value="rating_desc">Rating</option>
+        </select>
       {/* List */}
       <ul>
         {products.map((p) => (
@@ -76,6 +101,25 @@ useEffect(() => {
           </li>
         ))}
       </ul>
+      <div style={{ marginTop: 20 }}>
+        <button
+          disabled={pagination.current_page === 1}
+          onClick={() => goToPage(pagination.current_page - 1)}
+        >
+          Prev
+        </button>
+
+        <span>
+          Page {pagination.current_page} of {pagination.last_page}
+        </span>
+
+        <button
+          disabled={pagination.current_page === pagination.last_page}
+          onClick={() => goToPage(pagination.current_page + 1)}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
